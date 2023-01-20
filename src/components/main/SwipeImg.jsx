60221b272c62198f2d330241e947b29cfcbb477c
Kamalov <71275715+KamolovFarrux2005@users.noreply.github.com>
2023-01-20 06:48:19 +0500

    Update SwipeImg.jsx

--- src/components/main/SwipeImg.jsx
+++ src/components/main/SwipeImg.jsx
@@ -1,23 +1,23 @@
-// import { Swiper } from "swiper/react";
-// import { Navigation, Pagination } from "swiper";
+import { Swiper } from "swiper/react";
+import { Navigation, Pagination } from "swiper";
 
-// import "swiper/css";
-// import "swiper/css/pagination";
+import "swiper/css";
+import "swiper/css/pagination";
 
-// const SwipeImg = ({items}) => {
+const SwipeImg = ({items}) => {
 
-//     return (
-//         <div>
-//             <Swiper
-//                 pagination={true}
-//                 navigation={true}
-//                 modules={[Navigation, Pagination]}
-//                 className="mySwiper"
-//               >
-//                 {items}
-//             </Swiper>
-//         </div>
-//     )
-// }
+    return (
+        <div>
+            <Swiper
+                pagination={true}
+                navigation={true}
+                modules={[Navigation, Pagination]}
+                className="mySwiper"
+              >
+                {items}
+            </Swiper>
+        </div>
+    )
+}
 
-// export default SwipeImg
+export default SwipeImg
